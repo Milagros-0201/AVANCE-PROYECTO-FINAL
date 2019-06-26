44b82af2117e14932bb7438f943032ad90eacff0
Milagros Sanchez Chavez <milagrosyeneiasanchezchavez@gmail.com>
2019-06-26 09:55:57 -0500

    actualizar cambios

---
+++ ejercicio10.js
@@ -0,0 +1,81 @@
+const http=require('http');
+const url=require('url');
+const fs=require('fs');
+const querystring = require('querystring');
+
+const mime = {
+  html: 'text/html',
+  txt: 'text/plain',
+  css: 'text/css',
+  gif: 'image/gif',
+  jpg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  js: 'application/javascript'
+};
+
+const servidor=http.createServer((pedido ,respuesta) => {
+    const objetourl = url.parse(pedido.url);
+  let camino='public/colegio'+objetourl.pathname;
+  if (camino=='public/colegio/')
+    camino='colegio/index.html';
+  encaminar(pedido,respuesta,camino);
+});
+
+servidor.listen(8888);
+
+
+function encaminar (pedido,respuesta,camino) {
+  console.log(camino);
+  switch (camino) {
+    case 'public/colegio/': {
+      recuperar(pedido,respuesta);
+      break;
+    }	
+    default : {  
+      fs.stat(camino, error => {
+        if (!error) {
+        fs.readFile(camino,(error, contenido) => {
+          if (error) {
+            respuesta.writeHead(500, {'Content-Type': 'text/plain'});
+            respuesta.write('Error interno');
+            respuesta.end();					
+          } else {
+            const vec = camino.split('.');
+            const extension=vec[vec.length-1];
+            const mimearchivo=mime[extension];
+            respuesta.writeHead(200, {'Content-Type': mimearchivo});
+            respuesta.write(contenido);
+            respuesta.end();
+          }
+        });
+      } else {
+        respuesta.writeHead(404, {'Content-Type': 'text/html'});
+        respuesta.write('<!doctype html><html><head></head><body>Recurso inexistente</body></html>');		
+        respuesta.end();
+        }
+      });	
+    }
+  }	
+}
+
+
+function recuperar(pedido,respuesta) {
+  let info = '';
+  pedido.on('data', datosparciales => {
+    info += datosparciales;
+  });
+  pedido.on('end', () => {
+    const formulario = querystring.parse(info);
+    respuesta.writeHead(200, {'Content-Type': 'text/html'});
+    const pagina=
+      `<!doctype html><html><head></head><body>
+       Nombre de usuario:${formulario['nombre']}<br>
+      Clave:${formulario['clave']}<br>
+      <a href="index.html">Retornar</a>
+      </body></html>`;
+    respuesta.end(pagina);
+  });	
+}
+
+console.log('Servidor web iniciado');
